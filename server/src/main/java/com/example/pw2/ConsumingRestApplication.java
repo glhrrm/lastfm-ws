a98a089333a1f148fb38baac51462eb66addb158
gui.parzianello <glhrm_@live.com>
2019-11-24 22:58:45 -0300

    ajustes para gerar .war

--- server/src/main/java/com/example/pw2/ConsumingRestApplication.java
+++ server/src/main/java/com/example/pw2/ConsumingRestApplication.java
@@ -2,29 +2,18 @@ package com.example.pw2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConsumingRestApplication {
+public class ConsumingRestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumingRestApplication.class, args);
 	}
 
-	// @Bean
-	// public RestTemplate restTemplate(RestTemplateBuilder builder) {
-	// 	return builder.build();
-	// }
-
-	// @Bean
-	// public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-	// 	URL source = new URL("http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=since93&api_key=40824a562c9ab20ff7d4d15766b8b52a&format=json");
-	// 	JsonNode finalNode = new ObjectMapper().readTree(source).get("topalbums").get("album");
-
-	// 	return args -> {
-	// 		for (JsonNode objNode : finalNode) {
-	// 			Album album = new ObjectMapper().treeToValue(objNode, Album.class);
-	// 			System.out.println(album);
-	// 		}
-	// 	};
-	// }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+	  return builder.sources(ConsumingRestApplication.class);
+	}
 }
