2b1cb55079ab222d24390c303384bd3b2c716891
Guilherme Parzianello <glhrm_@live.com>
2019-11-27 17:29:19 -0300

    cliente adicionado

---
+++ client/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+     <section class="hero is-primary is-bold is-fullheight">
+      <!-- Hero content: will be in the middle -->
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="row">
+            <div class="columns">
+              <div class="column is-half is-offset-one-quarter">
+                <username></username>
+                <fetch></fetch>
+                <period></period>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Username from "./components/Username";
+import Period from "./components/Period";
+import Fetch from "./components/Fetch";
+
+export default {
+  name: "app",
+  components: {
+    Username,
+    Period,
+    Fetch
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ client/src/components/Period.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="select">
+    <select v-model="period">
+      <option value="overall">Overall</option>
+      <option value="7days">7 days</option>
+      <option value="1month">1 month</option>
+      <option value="3months">3 months</option>
+      <option value="6months">6 months</option>
+      <option value="12months">12 months</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "period",
+  data() {
+    return {
+      period: "overall"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.select {
+  border-left: 0;
+  border-right: 0;
+  border-top: 0;
+  border-radius: 0;
+  outline: none;
+  box-shadow: none;
+}
+.input.is-primary:focus {
+  box-shadow: none;
+}
+.add-button {
+  border-radius: 50%;
+}
+</style>
