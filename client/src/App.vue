6638d6b92a49972c37eb7a0d1fd93dbd094d4261
gui.parzianello <glhrm_@live.com>
2019-12-03 22:15:02 -0300

    cors permitido no backend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <div id="app">
-    <div class="inline">
-      <Logo />
+    <div id="app">
+        <div class="inline">
+            <Logo />
+        </div>
+        <div class="inline">
+            <Username v-on:inputData="updateUsername" />
+        </div>
+        <div class="inline">
+            <Period v-on:inputData="updatePeriod" />
+            <Search v-bind:username="username" v-bind:period="period" />
+        </div>
+        <div class="inline">
+            <AlbumList
+                v-bind:username="username"
+                v-bind:period="period"
+            />
+        </div>
     </div>
-    <div class="inline">
-      <Username v-on:inputData="updateUsername" />
-    </div>
-    <div class="inline">
-      <Period v-on:inputData="updatePeriod" />
-      <Search v-bind:username="username" v-bind:period="period" />
-    </div>
-    <div class="inline"><AlbumList v-bind:username="username" v-bind:period="period" /></div>
-  </div>
 </template>
 
 <script>
@@ -22,28 +27,28 @@ import Search from "./components/Search";
 import AlbumList from "./components/AlbumList";
 
 export default {
-  components: {
-    Logo,
-    Username,
-    Period,
-    AlbumList,
-    Search
-  },
-  data() {
-    return {
-      username: "",
-      period: "",
-      topAlbums: ""
-    };
-  },
-  methods: {
-    updateUsername(variable) {
-      this.username = variable;
+    components: {
+        Logo,
+        Username,
+        Period,
+        AlbumList,
+        Search
     },
-    updatePeriod(variable) {
-      this.period = variable;
+    data() {
+        return {
+            username: "",
+            period: "",
+            topAlbums: ""
+        };
+    },
+    methods: {
+        updateUsername(variable) {
+            this.username = variable;
+        },
+        updatePeriod(variable) {
+            this.period = variable;
+        }
     }
-  }
 };
 </script>
 
@@ -51,22 +56,22 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700&display=swap");
 
 * {
-  box-sizing: border-box;
-  font-family: "IBM Plex Sans", sans-serif;
+    box-sizing: border-box;
+    font-family: "IBM Plex Sans", sans-serif;
 }
 
 #app {
-  background-color: #111;
-  background-image: url("./assets/background.png");
-  height: 100%;
-  padding: 50px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+    background-color: #111;
+    background-image: url("./assets/background.png");
+    height: 100%;
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 }
 
 .inline {
-  display: inline-block;
+    display: inline-block;
 }
 </style>
--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,55 +1,45 @@
 <template>
-  <a v-bind:href="submit" target="_blank">
-    <div id="button">
-      <i class="fa fa-code"></i>
-    </div>
-  </a>
+    <a v-bind:href="submit" target="_blank">
+        <div id="button">
+            <i class="fa fa-code"></i>
+        </div>
+    </a>
 </template>
 
 <script>
 export default {
-  props: {
-    username: String,
-    period: String
-  },
-  data() {
-    return {
-      topAlbums: []
-    };
-  },
-  computed: {
-    submit() {
-      if (this.username != "" && this.period != "") {
-        return (
-          "http://localhost:8080/" +
-          this.username +
-          "/top-albums/" +
-          this.period
-        );
-      }
+    props: {
+        username: String,
+        period: String
+    },
+    computed: {
+        submit() {
+            if (this.username != "" && this.period != "") {
+                return `http://localhost:8080/${this.username}/top-albums/${this.period}`;
+            }
+        }
     }
-  }
 };
 </script>
 
 <style>
 #button {
-  border: none;
-  padding: 10px;
-  margin: 10px;
-  text-align: center;
-  border-radius: 10px;
-  display: unset;
-  border: 1.5px solid #aaa;
-  color: #ccc;
-  background-color: transparent;
+    border: none;
+    padding: 10px;
+    margin: 10px;
+    text-align: center;
+    border-radius: 10px;
+    display: unset;
+    border: 1.5px solid #aaa;
+    color: #ccc;
+    background-color: transparent;
 }
 
 #button a {
-  color: #ccc;
+    color: #ccc;
 }
 
 #button:hover {
-  background-color: darkred;
+    background-color: darkred;
 }
 </style>
